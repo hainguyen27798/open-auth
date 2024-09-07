2f25aafb631d2ab77b6d365a26f3b26475b14dfc
harry Nguyen <hainguyen27798@gmail.com>
2024-09-07 21:26:22 +0700

    format code

--- workspaces/backend/database/migrations/20240829142532_user.sql
+++ workspaces/backend/database/migrations/20240829142532_user.sql
@@ -1,20 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE users (
-    id varchar(36) NOT NULL,
-    created_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    updated_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NULL,
-    status enum ('active', 'inActive', 'request') NOT NULL DEFAULT 'inActive',
-    social_provider enum ('google', 'linkedin') NULL,
-    image text NULL,
-    verify tinyint NOT NULL DEFAULT 0,
-    verification_code varchar(6) NULL,
+CREATE TABLE users
+(
+    id                varchar(36)                            NOT NULL,
+    created_at        timestamp(6)                           NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    updated_at        timestamp(6)                           NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    name              varchar(255)                           NOT NULL,
+    email             varchar(255)                           NOT NULL,
+    password          varchar(255)                           NULL,
+    status            enum ('active', 'inActive', 'request') NOT NULL DEFAULT 'inActive',
+    social_provider   enum ('google', 'linkedin')            NULL,
+    image             text                                   NULL,
+    verify            tinyint                                NOT NULL DEFAULT 0,
+    verification_code varchar(6)                             NULL,
     UNIQUE INDEX IDX_user_email (email),
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 -- +goose StatementEnd
 
 -- +goose Down
